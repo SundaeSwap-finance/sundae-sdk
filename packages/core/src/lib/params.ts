2c190d68adbf7225e74d037096b10f976cabe91e
Calvin Koepke <hello@calvinkoepke.com>
2023-04-18 15:08:54 -0600

    fix: mainnet stats url

--- packages/core/src/lib/params.ts
+++ packages/core/src/lib/params.ts
@@ -1,6 +1,6 @@
 import { TSupportedNetworks } from "../@types";
 
 export const providerBaseUrls: Record<TSupportedNetworks, string> = {
-  mainnet: "https://api.sundaeswap.finance/graphql",
+  mainnet: "https://stats.sundaeswap.finance/graphql",
   preview: "https://api.stats.preview.sundaeswap.finance/graphql",
 };
