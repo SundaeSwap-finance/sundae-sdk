8ed3f34de275fc112f226e0fda9abc51a17137ee
Calvin Koepke <hello@calvinkoepke.com>
2024-02-14 17:01:55 -0700

    chore: restore original test pipeline

--- test-results/check.sh
+++
@@ -1,7 +0,0 @@
-xmllint --noout test-results/jest-junit.xml
-  if grep -q '<failure' test-results/jest-junit.xml; then
-    echo "Tests failures detected in jest-junit.xml"
-    exit 1
-  else
-    echo "No test failures found in jest-junit.xml"
-  fi
