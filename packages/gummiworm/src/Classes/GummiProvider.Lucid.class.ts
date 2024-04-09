6aba14a9221ce26e8c2dd31b5ad2fb367de38f22
Calvin Koepke <hello@calvinkoepke.com>
2024-04-09 09:47:59 -0600

    chore: fix gummiworm provider

--- packages/gummiworm/src/Classes/GummiProvider.Lucid.class.ts
+++ packages/gummiworm/src/Classes/GummiProvider.Lucid.class.ts
@@ -1,44 +1,107 @@
-import request from "graphql-request";
 import {
   Credential,
   Delegation,
   OutRef,
+  PROTOCOL_PARAMETERS_DEFAULT,
   ProtocolParameters,
   Provider,
   UTxO,
 } from "lucid-cardano";
-import { GetUtxosQuery } from "./queries";
 
 export class GummiProvider implements Provider {
   constructor(public endpoint: string) {}
 
-  awaitTx(
+  async awaitTx(
     txHash: string,
     checkInterval?: number | undefined
-  ): Promise<boolean> {}
+  ): Promise<boolean> {
+    console.log(txHash, checkInterval);
+    // const query = `
+    //   query {
+    //     confirmTx(hash: $hash)
+    //   }
+    // `
 
-  getDatum(datumHash: string): Promise<string> {}
-
-  getDelegation(rewardAddress: string): Promise<Delegation> {}
-
-  getProtocolParameters(): Promise<ProtocolParameters> {}
-
-  getUtxoByUnit(unit: string): Promise<UTxO> {}
-
-  async getUtxos(addressOrCredential: string | Credential): Promise<UTxO[]> {
-    return await request<UTxO[]>(this.endpoint, GetUtxosQuery, {
-      variables: {
-        address: addressOrCredential,
-      },
-    });
+    return true;
   }
 
-  getUtxosByOutRef(outRefs: OutRef[]): Promise<UTxO[]> {}
+  async getDatum(datumHash: string): Promise<string> {
+    console.log(datumHash);
+    // const query = `
+    //   query {
+    //     getDatum(txHash: $txHash)
+    //   }
+    // `
 
-  getUtxosWithUnit(
-    addressOrCredential: string | Credential,
-    unit: string
-  ): Promise<UTxO[]> {}
+    return "";
+  }
 
-  submitTx(tx: string): Promise<string> {}
+  async getDelegation(rewardAddress: string): Promise<Delegation> {
+    console.log(rewardAddress);
+    return {
+      poolId: "",
+      rewards: 0n,
+    };
+  }
+
+  async getProtocolParameters(): Promise<ProtocolParameters> {
+    return PROTOCOL_PARAMETERS_DEFAULT;
+  }
+
+  async getUtxos(addressOrCredential: string | Credential): Promise<UTxO[]> {
+    // return await request<UTxO[]>(this.endpoint, GetUtxosQuery, {
+    //   variables: {
+    //     address: addressOrCredential,
+    //   },
+    // });
+    console.log(addressOrCredential);
+
+    return [];
+  }
+
+  async getUtxoByUnit(unit: string): Promise<UTxO> {
+    // const query = `
+    //   query {
+    //     getUtxosByUnit(assetId: $assetId) {
+    //       ...
+    //     }
+    //   }
+    // `
+    console.log(unit);
+
+    return {
+      address: "",
+      assets: {},
+      outputIndex: 0,
+      txHash: "",
+    };
+  }
+
+  async getUtxosByOutRef(outRefs: OutRef[]): Promise<UTxO[]> {
+    console.log(outRefs);
+    return [
+      {
+        address: "",
+        assets: {},
+        outputIndex: 0,
+        txHash: "",
+      },
+    ];
+  }
+
+  async getUtxosWithUnit(): Promise<UTxO[]> {
+    return [
+      {
+        address: "",
+        assets: {},
+        outputIndex: 0,
+        txHash: "",
+      },
+    ];
+  }
+
+  async submitTx(tx: string): Promise<string> {
+    console.log(tx);
+    return "";
+  }
 }
