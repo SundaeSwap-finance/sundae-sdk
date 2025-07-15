4b9448c2cc83ddd402edcaf997a677f0711f780d
Rob van Leeuwen <luivatra@gmail.com>
2025-07-15 19:23:19 +0200

    add extra nftcheck datum test cases

--- packages/core/src/DatumBuilders/__data__/NftCheck.expectations.ts
+++ packages/core/src/DatumBuilders/__data__/NftCheck.expectations.ts
@@ -13,5 +13,38 @@ export const NFTCHECK_EXPECTATIONS = {
         cbor: "d8799fd8799fa1581cfa3eff2047fdf9293c5feef4dc85ce58097ea1c6da4845a351535183a14574494e445901d87980ffff",
       },
     },
+    {
+      args: {
+        value: [new AssetAmount(1n, PREVIEW_DATA.assets.tindy.metadata)],
+        check: "Any",
+      } as IDatumBuilderNftCheckArgs,
+      expectations: {
+        cbor: "d8799fd8799fa1581cfa3eff2047fdf9293c5feef4dc85ce58097ea1c6da4845a351535183a14574494e445901d87a80ffff",
+      },
+    },
+    {
+      args: {
+        value: [
+          new AssetAmount(1n, PREVIEW_DATA.assets.tindy.metadata),
+          new AssetAmount(1n, PREVIEW_DATA.assets.usdc.metadata),
+        ],
+        check: "All",
+      } as IDatumBuilderNftCheckArgs,
+      expectations: {
+        cbor: "d8799fd8799fa2581cfa3eff2047fdf9293c5feef4dc85ce58097ea1c6da4845a351535183a14574494e445901581c99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15a1445553444301d87980ffff",
+      },
+    },
+    {
+      args: {
+        value: [
+          new AssetAmount(1n, PREVIEW_DATA.assets.tindy.metadata),
+          new AssetAmount(1n, PREVIEW_DATA.assets.usdc.metadata),
+        ],
+        check: "Any",
+      } as IDatumBuilderNftCheckArgs,
+      expectations: {
+        cbor: "d8799fd8799fa2581cfa3eff2047fdf9293c5feef4dc85ce58097ea1c6da4845a351535183a14574494e445901581c99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15a1445553444301d87a80ffff",
+      },
+    },
   ],
 };
--- packages/core/src/DatumBuilders/__tests__/DatumBuilder.NftCheck/buildConditionDatum.test.ts
+++ packages/core/src/DatumBuilders/__tests__/DatumBuilder.NftCheck/buildConditionDatum.test.ts
@@ -14,12 +14,36 @@ afterEach(() => {
 });
 
 describe("buildConditionDatum", () => {
-  it("should correctly build the condition datum for checking a token", () => {
+  it("should correctly build the condition datum for checking a single token using 'All'", () => {
     const result = builderInstance.buildConditionDatum(
       NFTCHECK_EXPECTATIONS.buildConditionDatum[0].args,
     );
     expect(result.toCbor().toString()).toEqual(
         NFTCHECK_EXPECTATIONS.buildConditionDatum[0].expectations.cbor,
+    );
+  });
+  it("should correctly build the condition datum for checking a single token using 'Any'", () => {
+    const result = builderInstance.buildConditionDatum(
+      NFTCHECK_EXPECTATIONS.buildConditionDatum[1].args,
+    );
+    expect(result.toCbor().toString()).toEqual(
+        NFTCHECK_EXPECTATIONS.buildConditionDatum[1].expectations.cbor,
+    );
+  });
+  it("should correctly build the condition datum for checking multiple tokens using 'All'", () => {
+    const result = builderInstance.buildConditionDatum(
+      NFTCHECK_EXPECTATIONS.buildConditionDatum[2].args,
+    );
+    expect(result.toCbor().toString()).toEqual(
+        NFTCHECK_EXPECTATIONS.buildConditionDatum[2].expectations.cbor,
+    );
+  });
+  it("should correctly build the condition datum for checking multiple tokens using 'Any'", () => {
+    const result = builderInstance.buildConditionDatum(
+      NFTCHECK_EXPECTATIONS.buildConditionDatum[3].args,
+    );
+    expect(result.toCbor().toString()).toEqual(
+        NFTCHECK_EXPECTATIONS.buildConditionDatum[3].expectations.cbor,
     );
   });
 });
