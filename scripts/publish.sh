984f1503a4a3768daae1d60913e967ba82c0d2c9
Calvin Koepke <hello@calvinkoepke.com>
2025-10-21 10:03:10 -0600

    feat: build scripts

---
+++ scripts/publish.sh
@@ -0,0 +1,5 @@
+// Publish each package manually via Bun, so that all "workspace:*" and "catalog:" versions
+// in the corresponding package.json file are resolved to their real versioning on the root.
+for dir in packages/*; do
+  (cd "$dir" && bun publish || true)
+done && changeset tag
