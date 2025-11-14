d9cd6e107fade20913bb716e36d01ee466870925
Calvin Koepke <hello@calvinkoepke.com>
2025-11-13 17:04:17 -0700

    fix: bun auth token variable name

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 # Publish each package manually via Bun, so that all "workspace:*" and "catalog:" versions
 # in the corresponding package.json file are resolved to their real versioning on the root.
 for dir in packages/*; do
-  if [[ "$dir" == "packages/demo" ]]; then
+  if [ "$dir" == "packages/demo" ]; then
     continue
   fi
   (cd "$dir" && bun publish || true)
